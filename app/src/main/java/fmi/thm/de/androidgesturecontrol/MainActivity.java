3cbdf240871f62d566b6c3f1ca75e7467a694359
Thomas Rehm <thomas.rehm89@gmail.com>
2015-07-22 10:32:18 +0200

    added image to fullscreen button

--- app/src/main/java/fmi/thm/de/androidgesturecontrol/MainActivity.java
+++ app/src/main/java/fmi/thm/de/androidgesturecontrol/MainActivity.java
@@ -47,7 +47,7 @@ public class MainActivity extends ActionBarActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_fullscreen) {
             return true;
         }
 
