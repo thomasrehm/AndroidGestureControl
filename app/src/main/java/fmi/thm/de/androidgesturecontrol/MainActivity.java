f7395a476fd357f372f8fe88eccf0bacc670228a
Thomas Rehm <thomas.rehm89@gmail.com>
2015-07-22 16:53:10 +0200

    add back button support and prevent app from restarting on orientation change

--- app/src/main/java/fmi/thm/de/androidgesturecontrol/MainActivity.java
+++ app/src/main/java/fmi/thm/de/androidgesturecontrol/MainActivity.java
@@ -3,6 +3,7 @@ package fmi.thm.de.androidgesturecontrol;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -83,5 +84,17 @@ public class MainActivity extends ActionBarActivity {
                 View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                         | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                         | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        // Check if the key event was the Back button and if there's history
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && mWebView.canGoBack()) {
+            mWebView.goBack();
+            return true;
+        }
+        // If it wasn't the Back key or there's no web page history, bubble up to the default
+        // system behavior (probably exit the activity)
+        return super.onKeyDown(keyCode, event);
     }
 }
