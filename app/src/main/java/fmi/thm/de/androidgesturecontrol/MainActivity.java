a5bc20dfa43e9af8c805f9b1ccaf11147f62b7b0
Saskia Schreiber <sas.schreiber@gmail.com>
2015-09-29 19:54:03 +0200

    [FEATURE] Move gesture code to separate js file

--- app/src/main/java/fmi/thm/de/androidgesturecontrol/MainActivity.java
+++ app/src/main/java/fmi/thm/de/androidgesturecontrol/MainActivity.java
@@ -1,5 +1,6 @@
 package fmi.thm.de.androidgesturecontrol;
 
+import android.content.res.AssetManager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -15,6 +16,12 @@ import org.xwalk.core.XWalkNavigationHistory;
 import org.xwalk.core.XWalkPreferences;
 import org.xwalk.core.XWalkView;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 public class MainActivity extends ActionBarActivity {
 
     private XWalkView xWalkWebView;
@@ -23,253 +30,7 @@ public class MainActivity extends ActionBarActivity {
     private boolean btn_enable_gesture_clicked = false;
     private String rm_script = "document.body.removeChild(document.getElementById('video'));" +
             "document.body.removeChild(document.getElementById('canvas'));";
-    private String add_script = "var vid = document.createElement('video'),\n" +
-            "can = document.createElement('canvas');\n" +
-            "vid.setAttribute('id','video');\n" +
-            "vid.setAttribute('width', '300');\n" +
-            "vid.setAttribute('style', 'display:none');\n" +
-            "vid.setAttribute('autoplay', 'autoplay');\n" +
-            "can.setAttribute('id', 'canvas');\n" +
-            "can.setAttribute('style', 'width:300px;display:none;');\n" +
-            "\n" +
-            "document.body.appendChild(vid);\n" +
-            "document.body.appendChild(can);\n" +
-            "console.log('injected video and canvas element');\n" +
-            "setTimeout(gesture, 1000);\n" +
-            "function gesture(){\n" +
-            "    \"use strict\";\n" +
-            "\n" +
-            "    var doc = window.document,\n" +
-            "        draw,\n" +
-            "        video = doc.getElementById('video'),\n" +
-            "        canvas = doc.getElementById('canvas'),\n" +
-            "        _ = canvas.getContext('2d'),\n" +
-            "        // ccanvas = doc.getElementById('comp'),\n" +
-            "        // c_ = ccanvas.getContext('2d'),\n" +
-            "        compression = 5,\n" +
-            "        height = 0,\n" +
-            "        width = 0,\n" +
-            "        huemin = 0.0,\n" +
-            "        huemax = 0.1,\n" +
-            "        satmin = 0.0,\n" +
-            "        satmax = 1.0,\n" +
-            "        valmin = 0.4,\n" +
-            "        valmax = 1.0,\n" +
-            "        last = false,\n" +
-            "        thresh = 150,\n" +
-            "        down = false,\n" +
-            "        wasdown = false,\n" +
-            "        movethresh = 4,\n" +
-            "        brightthresh = 300,\n" +
-            "        overthresh = 1000,\n" +
-            "        avg = 0,\n" +
-            "        state = 0;\n" +
-            "    // States:\n" +
-            "    // 0 waiting for gesture\n" +
-            "    // 1 waiting for next move after gesture\n" +
-            "    // 2 waiting for gesture to end\n" +
-            "\n" +
-            "    navigator.webkitGetUserMedia({\n" +
-            "        audio: false,\n" +
-            "        video: true\n" +
-            "    }, function (stream) {\n" +
-            "        video.src = window.URL.createObjectURL(stream);\n" +
-            "        video.addEventListener('play',\n" +
-            "            function () {\n" +
-            "                setInterval(dump, 1000 / 25);\n" +
-            "            });\n" +
-            "    }, function () {\n" +
-            "        throw new Error('OOOOOOOH! DEEEEENIED!');\n" +
-            "    });\n" +
-            "\n" +
-            "    function dump() {\n" +
-            "        if (canvas.width != video.videoWidth) {\n" +
-            "            width = Math.floor(video.videoWidth / compression);\n" +
-            "            height = Math.floor(video.videoHeight / compression);\n" +
-            "            canvas.width = width;\n" +
-            "            canvas.height = height;\n" +
-            "            // ccanvas.width = width;\n" +
-            "            // ccanvas.height = height;\n" +
-            "        }\n" +
-            "        _.drawImage(video, width, 0, -width, height);\n" +
-            "        draw = _.getImageData(0, 0, width, height);\n" +
-            "        //c_.putImageData(draw,0,0);\n" +
-            "        skinfilter();\n" +
-            "        test();\n" +
-            "    }\n" +
-            "\n" +
-            "    function skinfilter() {\n" +
-            "        var skin_filter = _.getImageData(0, 0, width, height),\n" +
-            "            total_pixels = skin_filter.width * skin_filter.height,\n" +
-            "            index_value = total_pixels * 4,\n" +
-            "            count_data_big_array = 0;\n" +
-            "        for (var y = 0; y < height; y++) {\n" +
-            "            for (var x = 0; x < width; x++) {\n" +
-            "                index_value = x + y * width;\n" +
-            "                var r = draw.data[count_data_big_array],\n" +
-            "                    g = draw.data[count_data_big_array + 1],\n" +
-            "                    b = draw.data[count_data_big_array + 2],\n" +
-            "                    a = draw.data[count_data_big_array + 3];\n" +
-            "\n" +
-            "                var hsv = rgb2Hsv(r, g, b);\n" +
-            "                //When the hand is too lose (hsv[0] > 0.59 && hsv[0] < 1.0)\n" +
-            "                //Skin Range on HSV values\n" +
-            "                if (((hsv[0] > huemin && hsv[0] < huemax) || (hsv[0] > 0.59 && hsv[0] < 1.0)) && (hsv[1] > satmin && hsv[1] < satmax) && (hsv[2] > valmin && hsv[2] < valmax)) {\n" +
-            "                    skin_filter[count_data_big_array] = r;\n" +
-            "                    skin_filter[count_data_big_array + 1] = g;\n" +
-            "                    skin_filter[count_data_big_array + 2] = b;\n" +
-            "                    skin_filter[count_data_big_array + 3] = a;\n" +
-            "                } else {\n" +
-            "                    skin_filter.data[count_data_big_array] =\n" +
-            "                        skin_filter.data[count_data_big_array + 1] =\n" +
-            "                        skin_filter.data[count_data_big_array + 2] = 0;\n" +
-            "                    skin_filter.data[count_data_big_array + 3] = 0;\n" +
-            "                }\n" +
-            "\n" +
-            "                count_data_big_array = index_value * 4;\n" +
-            "            }\n" +
-            "        }\n" +
-            "        draw = skin_filter;\n" +
-            "    }\n" +
-            "\n" +
-            "    function rgb2Hsv(r, g, b) {\n" +
-            "        r = r / 255;\n" +
-            "        g = g / 255;\n" +
-            "        b = b / 255;\n" +
-            "        var max = Math.max(r, g, b),\n" +
-            "            min = Math.min(r, g, b),\n" +
-            "            h, s, v = max,\n" +
-            "            d = max - min;\n" +
-            "        s = max === 0 ? 0 : d / max;\n" +
-            "\n" +
-            "        if (max == min) {\n" +
-            "            h = 0; // achromatic\n" +
-            "        } else {\n" +
-            "\n" +
-            "            switch (max) {\n" +
-            "            case r:\n" +
-            "                h = (g - b) / d + (g < b ? 6 : 0);\n" +
-            "                break;\n" +
-            "            case g:\n" +
-            "                h = (b - r) / d + 2;\n" +
-            "                break;\n" +
-            "            case b:\n" +
-            "                h = (r - g) / d + 4;\n" +
-            "                break;\n" +
-            "            }\n" +
-            "            h /= 6;\n" +
-            "        }\n" +
-            "\n" +
-            "        return [h, s, v];\n" +
-            "    }\n" +
-            "\n" +
-            "    function test() {\n" +
-            "        var delt = _.createImageData(width, height),\n" +
-            "            totalx, totaly, totald, totaln, dscl, pix;\n" +
-            "        if (last !== false) {\n" +
-            "            totalx = 0;\n" +
-            "            totaly = 0;\n" +
-            "            totald = 0;\n" +
-            "            totaln = delt.width * delt.height;\n" +
-            "            dscl = 0;\n" +
-            "            pix = totaln * 4;\n" +
-            "            while (pix -= 4) {\n" +
-            "                var d = Math.abs(draw.data[pix] - last.data[pix]) + Math.abs(draw.data[pix + 1] - last.data[pix + 1]) + Math.abs(draw.data[pix + 2] - last.data[pix + 2]);\n" +
-            "                if (d > thresh) {\n" +
-            "                    delt.data[pix] = 160;\n" +
-            "                    delt.data[pix + 1] = 255;\n" +
-            "                    delt.data[pix + 2] =\n" +
-            "                        delt.data[pix + 3] = 255;\n" +
-            "                    totald += 1;\n" +
-            "                    totalx += ((pix / 4) % width);\n" +
-            "                    totaly += (Math.floor((pix / 4) / delt.height));\n" +
-            "                } else {\n" +
-            "                    delt.data[pix] =\n" +
-            "                        delt.data[pix + 1] =\n" +
-            "                        delt.data[pix + 2] = 0;\n" +
-            "                    delt.data[pix + 3] = 0;\n" +
-            "                }\n" +
-            "            }\n" +
-            "        }\n" +
-            "        //slide.setAttribute('style','display:initial')\n" +
-            "        //slide.value=(totalx/totald)/width\n" +
-            "        if (totald) {\n" +
-            "            down = {\n" +
-            "                x: totalx / totald,\n" +
-            "                y: totaly / totald,\n" +
-            "                d: totald\n" +
-            "            };\n" +
-            "            handledown();\n" +
-            "        }\n" +
-            "        //console.log(totald)\n" +
-            "        last = draw;\n" +
-            "        //c_.putImageData(delt, 0, 0);\n" +
-            "    }\n" +
-            "\n" +
-            "    function calibrate() {\n" +
-            "        wasdown = {\n" +
-            "            x: down.x,\n" +
-            "            y: down.y,\n" +
-            "            d: down.d\n" +
-            "        };\n" +
-            "    }\n" +
-            "\n" +
-            "    function handledown() {\n" +
-            "        avg = 0.9 * avg + 0.1 * down.d;\n" +
-            "        var davg = down.d - avg,\n" +
-            "            good = davg > brightthresh;\n" +
-            "        //console.log(davg)\n" +
-            "        switch (state) {\n" +
-            "        case 0:\n" +
-            "            if (good) { //Found a gesture, waiting for next move\n" +
-            "                console.log(\"Waiting for gesture\");\n" +
-            "                state = 1;\n" +
-            "                calibrate();\n" +
-            "            }\n" +
-            "            break;\n" +
-            "        case 2: //Wait for gesture to end\n" +
-            "            if (!good) { //Gesture ended\n" +
-            "                console.log(\"Gesture ended.\");\n" +
-            "                console.log(\"--------------------------------\");\n" +
-            "                state = 0;\n" +
-            "            }\n" +
-            "            break;\n" +
-            "        case 1: //Got next move, do something based on direction\n" +
-            "            var dx = down.x - wasdown.x,\n" +
-            "                dy = down.y - wasdown.y;\n" +
-            "            var isHorizontalMovement = Math.abs(dy) < Math.abs(dx); //(dx,dy) is on a bowtie\n" +
-            "            console.log(\"dx: \" + dx);\n" +
-            "            console.log(\"isHorizontalMovement: \" + isHorizontalMovement);\n" +
-            "            //console.log(good,davg)\n" +
-            "            if (dx < -movethresh && isHorizontalMovement) {\n" +
-            "                console.log('right');\n" +
-            "                Reveal.navigateRight();\n" +
-            "            } else if (dx > movethresh && isHorizontalMovement) {\n" +
-            "                console.log('left');\n" +
-            "                Reveal.navigateLeft();\n" +
-            "            }\n" +
-            "            if (dy > movethresh && !isHorizontalMovement) {\n" +
-            "                if (davg > overthresh) {\n" +
-            "                    console.log('over up');\n" +
-            "                    Reveal.toggleOverview();\n" +
-            "                } else {\n" +
-            "                    console.log('up');\n" +
-            "                    Reveal.navigateUp();\n" +
-            "                }\n" +
-            "            } else if (dy < -movethresh && !isHorizontalMovement) {\n" +
-            "                if (davg > overthresh) {\n" +
-            "                    console.log('over down');\n" +
-            "                    Reveal.toggleOverview();\n" +
-            "                } else {\n" +
-            "                    console.log('down');\n" +
-            "                    Reveal.navigateDown();\n" +
-            "                }\n" +
-            "            }\n" +
-            "            state = 2;\n" +
-            "            break;\n" +
-            "        }\n" +
-            "    }\n" +
-            "}\n";
+    private String add_script;
 
     private static  String TAG = "AGC";
 
@@ -279,6 +40,34 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
 
         mDecorView = getWindow().getDecorView();
+        AssetManager am = getAssets();
+/*        String[] list;
+        try {
+            list = am.list("js");
+            for (String listelement : list) {
+                Log.d(TAG, "AssetManager: found " + listelement);
+            }
+        } catch (IOException e) {
+            Log.e(TAG, "Could not list folders from AssetManager");
+        }*/
+
+
+
+        try {
+            InputStream inputStream = am.open("gesture.js", am.ACCESS_BUFFER);
+
+            if ( inputStream != null ) {
+                add_script = convertStreamToString(inputStream);
+                Log.v(TAG, "add_script length: " + add_script.length());
+            }
+        }
+        catch (FileNotFoundException e) {
+            Log.e(TAG, "File not found: " + e.toString());
+        } catch (IOException e) {
+            Log.e(TAG, "Can not read file: " + e.toString());
+        }
+
+
 
         xWalkWebView = (XWalkView) findViewById(R.id.xwalkWebView);
         xWalkWebView.clearCache(true);
@@ -390,5 +179,10 @@ public class MainActivity extends ActionBarActivity {
         // If it wasn't the Back key or there's no web page history, bubble up to the default
         // system behavior (probably exit the activity)
         return super.onKeyDown(keyCode, event);
+    }
+
+    static String convertStreamToString(java.io.InputStream is) {
+        java.util.Scanner s = new java.util.Scanner(is).useDelimiter("\\A");
+        return s.hasNext() ? s.next() : "";
     }
 }
